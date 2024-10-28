7d8a0b595adfdffdf9bd73a4a29f58dc8fc8c366
Vitor <157237604+SoldadoHumano@users.noreply.github.com>
2024-10-27 21:29:01 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int borderWidth = 360;
+        int borderHeight = 640;
+        Image icon = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/flappybird.png"));
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setSize(borderWidth, borderHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setIconImage(icon);
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
+}
